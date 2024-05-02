0214c45c8ff21d188d3ae82ff49e21477e42a8e2
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2024-05-02 14:56:19 +0200

    basic configuration of discord rpc applicaiton

---
+++ src/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const clientId = process.env.USER_ID;
+const DiscordRPC = require('discord-rpc');
+const RPC = new DiscordRPC.Client({ transport: 'ipc'});
+
+DiscordRPC.register(clientId);
+
+async function setActivity() {
+    if (!RPC) return;
+    RPC.setActivity({
+        details: `Testing presence`,
+        state: `Playing with RPC`,
+        startTimestamp: Date.now(),
+        largeImageKey: '18jfiprg',
+        largeImageText: `Discord Icon`,
+        smallImageKey: '18jfiprg',
+        smallImageText: `Small Icon`,
+        instance: false,
+        buttons: [
+            {
+                label: `Testing Button`,
+                url: `https://www.google.com`
+            }
+        ]
+    });
+};
+
+RPC.on('ready', async () => {
+    setActivity();
+
+    setInterval(() => {
+        setActivity();
+    }, 15 * 1000);
+});
+
+RPC.login({ clientId }).catch(console.error);
